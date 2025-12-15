191810387cc0687f8e6dd3b07fa71d6b895a28f7
iain-eng <iain@reorientplaces.com>
2025-12-15 15:50:14 +0000

    Change static file serving to current directory
    Updated static file serving to use the current directory.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // Serve frontend files from 'public' folder
+app.use(express.static(__dirname)); // Serve frontend files from 'public' folder
 
 // Data file paths
 const DATA_DIR = path.join(__dirname, "data");
